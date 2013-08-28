c9bcac57600b9dccb206ccfcfc46b1bb5cde1134
Luke Wren <wren6991@gmail.com>
2013-08-28 01:36:07 +0100

    38kHz carrier generated in software, removed serial control

--- ir_remote/ir_remote.ino
+++ ir_remote/ir_remote.ino
@@ -1,60 +1,82 @@
-#define BUFFER_LENGTH 128
+// PORTD: bit 7 -> |Bt7|Bt6|Bt5|Bt4|Bt3|Bt2|Bt1|Bt0| -> bit 0
+// PORTC: bit 5 ->         | - | - |Lrn|LED|IRo|IRi|
+
+#define BUFFER_LENGTH 64
 uint8_t irbuffer[BUFFER_LENGTH];
+bool irout_enable;
 
 void setup()
 {
-  Serial.begin(9600);
-  DDRC = 0x02;
+  DDRC = 0x06;    // led and ir out are configured for output
+  PORTC = 0x08;   // pullup enabled for learn button
+  DDRD = 0x00;    // all buttons are inputs
+  PORTD = 0xff;   // all pullups enabled
+  for (int i = 0; i < 6; i++)
+  {
+    PORTC ^= 0x04;
+    delay(100);
+  }
+  irout_enable = false;
+  noInterrupts();
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TCNT1 = 0;
+  
+  OCR1A = 210;          // compare match register 16MHz/210 = 76kHz
+  TCCR1B |= 1 << WGM12;  // CTC mode
+  TCCR1B |= 1 << CS10;   // no prescale
+  TIMSK1 |= 1 << OCIE1A; // enable timer output compare interrupt
+  interrupts();
+}
+
+ISR(TIMER1_COMPA_vect)
+{
+  if (irout_enable)
+    PORTC ^= 0x02;
+  else
+    PORTC &`= ~0x02;
 }
 
 void loop()
 {
-  if (Serial.available() > 0)
+  if (PIND == 0xff) // no function buttons pressed
+    return;
+  uint8_t function_number = 0;
+  while (!((PIND >> function_number) & 0x01))
+    function_number++;
+  if (PINC & 0x08) // learn button not pressed
   {
-    char command = Serial.read();
-    if (command == 'r')
+    PINC |= 0x04; // switch LED on
+    for (uint8_t i = 0; i < BUFFER_LENGTH; i++)
     {
-      Serial.println("Waiting for command...");
-      while (PINC & 0x01);
-      for (uint8_t i = 0; i < BUFFER_LENGTH; i++)
+      uint8_t c = irbuffer[i];
+      for (uint8_t bitcount = 0; bitcount < 8; bitcount++)
       {
-        uint8_t c = 0;
-        for (uint8_t count = 0; count < 8; count++)
-        {
-          c = (c << 1) | (PINC & 0x01);
-          delayMicroseconds(100);
-        }
-        irbuffer[i] = ~c;
-      }
-      for (uint8_t i = 0; i < BUFFER_LENGTH; i++)
-      {
-        if (irbuffer[i] < 0x10)
-          Serial.print("0");
-         Serial.print((uint8_t)(irbuffer[i]), HEX);
-         if (i % 16 == 15)
-           Serial.println("");
+        irout_enable = c & 0x80;
+        c = c << 1;
+        delayMicroseconds(100);
       }
     }
-    else if (command == 't')
-    {
-      Serial.print("Transmitting command... ");
-      for (uint8_t i = 0; i < BUFFER_LENGTH; i++)
-      {
-        uint8_t c = irbuffer[i];
-        for (uint8_t bitcount = 0; bitcount < 8; bitcount++)
-        {
-          PORTC = (PORTC & ~0x02) | ((c & 0x80) >> 6);
-          c = c << 1;
-          delayMicroseconds(100);
-        }
-      }
-      PORTC = (PORTC & ~0x02);
-      Serial.println("Done.");
-    }
-    else
-    {
-      Serial.print("Unrecognized: ");
-      Serial.println(command);
-    }
+    PORTC = PORTC & ~0x04;  // switch off LED and IRout
+    irout_enable = false;
   }
+  else // learn button is pressed
+  {
+    while (PINC & 0x01)
+      PORTC ^= 0x04;
+    PORTC |= 0x04;
+    for (uint8_t i = 0; i < BUFFER_LENGTH; i++)
+    {
+      uint8_t c = 0;
+      for (uint8_t count = 0; count < 8; count++)
+      {
+        c = (c << 1) | (PINC & 0x01);
+        delayMicroseconds(100);
+      }
+      irbuffer[i] = ~c;
+    }
+    delay(100);
+    PORTC &= ~0x04;
+  }
+  delay(20);
 }
